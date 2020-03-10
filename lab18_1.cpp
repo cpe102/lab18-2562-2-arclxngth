f27f90f8a6a499ee1fe26295927b66f3ea0505c8
arclxngth <59277471+arclxngth@users.noreply.github.com>
2020-03-10 17:57:28 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -6,7 +6,7 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(Rect,Rect);
+double overlap(Rect , Rect );
 
 int main(){
 
@@ -15,47 +15,54 @@ int main(){
 	cout << "Please input Rect 1 (x y w h): ";
 	cin>>x>>y>>w>>h;
 	
-	Rect r1 = {x,y,w,h};
+	Rect rect1 = {x,y,w,h};
 
 	cout << "Please input Rect 2 (x y w h): ";
 	cin>>x>>y>>w>>h;
 
-	Rect r2 = {x,y,w,h};
+	Rect rect2 = {x,y,w,h};
 	
-	cout << "Overlap area = "<<overlap(r1,r2);	
+	cout << "Overlap area = "<<overlap(rect1,rect2);	
 
 	return 0;
 }
 
-double overlap(Rect r1, Rect r2){
+double overlap(Rect rect1, Rect rect2) {
+    double overlapW, overlapH;
+    
+    // swap X
+    if (rect1.x > rect2.x) {
+        double t = rect2.x;
+        rect2.x = rect1.x;
+        rect1.x = t;
+        
+        t = rect2.w;
+        rect2.w = rect1.w;
+        rect1.w = t;
+        
+        t = rect2.y;
+        rect2.y = rect1.y;
+        rect1.y = t;
+        
+        t = rect2.h;
+        rect2.h = rect1.h;
+        rect1.h = t;
+    }
+    
+    if (rect1.x + rect1.w > rect2.x) {
+        overlapW = rect1.x + rect1.w - rect2.x;
+        
+        if (overlapW > rect2.w) overlapW = rect2.w;
+    }
+    else return 0;
+    
+    if (rect1.y + rect1.h > rect2.y) {
+        overlapH = rect1.y + rect1.h - rect2.y;
+        
+        if (overlapH > rect2.h) overlapH = rect2.h;
+    }
+    else return 0;
 
-	float x_min, y_min, x_diff, y_diff, w_max, h_max;
-
-	x_diff = abs(r1.x-r2.x);
-	y_diff = abs(r1.y-r2.y);
-
-	if(r1.w>r2.w)	w_max = r1.w;
-	else			w_max = r2.w;
-
-	if(r1.h>r1.h)	h_max = r1.h;
-	else 			h_max = r2.h;
-
-	if(r1.x<r2.x){
-		x_min=r1.x+r1.w;
-		y_min=r1.y+r1.h;
-
-		if(x_min < r2.x)	return 0;
-		else if(y_min < r2.y)	return 0;
-		else if(x_min > r2.x+r2.w && y_min > r2.y+r2.h)	return abs(r2.w*r2.h);
-		else return abs((w_max-x_diff)*(h_max-y_diff));
-	}
-	else if(r1.x>r2.x){
-		x_min = r2.x+r2.w;
-		y_min = r2.y+r2.h;
-
-		if(x_min < r1.x)	return 0;
-		else if(y_min < r1.y)	return 0;
-		else if(x_min > r1.x+r1.w && y_min > r1.y+r1.h)	return abs(r1.w*r1.h);
-		else return abs((w_max-x_diff)*(h_max-y_diff));
-	}
+	return overlapW * overlapH;
 }
+
